81e653cc7dc831dafb0c471e2938c29ba3f30e38
Reus99 <berkoncu99@gmail.com>
2022-11-29 23:13:47 +0300

    Request

--- login.js
+++ login.js
@@ -1,12 +1,22 @@
 const girisBtn = document.getElementById('girisYap');
-const api_url_CE = "http://localhost:8085/";
+const apiUrl = "http://localhost:8085/v1/kullanici";
+const getAllKullaniciUrl = `${apiUrl}/all`;
+const kullaniciSilUrl = `${apiUrl}/delete`;
+const loginUrl = `${apiUrl}/login`;
+const loginUsername = document.getElementById("login_user");
+const loginPassword = document.getElementById("login_pass");
 
-
-girisBtn.addEventListener('click',()=>{
-    axios.get(`${api_url_CE} + v1/kullaniciLogin/getAllKullanici`)
+girisBtn.addEventListener('click', () => {
+  // axios.METHOD(url, data)
+  // method => GET, POST, PUT, 
+  // data = {a: "a", b: "b"}
+  const data = {
+    kullaniciAdi: loginUsername.value,
+    sifre:loginPassword.value
+  };
+  axios.post(loginUrl, data)
     .then(response => {
-     const users = response.data;
-     console.log(`GET users`, users);
-   })
-    .catch(error => console.error(error));
+      const users = response.data;
+      console.log(`GET users`, users);
+    });
 });
